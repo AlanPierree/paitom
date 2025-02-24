30aa1c90204451ae8c0f8d4f1fb76fe88c374042
Alan <161715146+AlanPierree@users.noreply.github.com>
2025-02-24 08:51:10 -0300

    Add files via upload

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,20 +1,39 @@
-print("*********************************")
-print("Bem vindo ao jogo de Adivinhação!")
-print("*********************************\n")
+# https://docs.python.org/3/library/string.html#formatexamples
 
-pais = "Brasil"
-nome = "Alan"
-type(pais)
-type(nome)
-print("Seu nome é", nome, "e você é do", pais, "\n")
+total_de_tentativas = 3
+rodada = 1
 
-numero_secreto = 42
+for rodada in range(1, total_de_tentativas + 1):
+    print("\n*********************************")
+    print("Bem vindo ao jogo de Adivinhação!")
+    print("*********************************")
 
-chute_str = input("Digite o seu número: ")
-print("Você digitou: ", chute_str)
-chute = int(chute_str)
+    print("\nTentativa {} de {}\n".format(rodada, total_de_tentativas))
 
-if (numero_secreto == chute):
-    print("Você acertou!\n")
-else:
-    print("Você errou!\n")
+    numero_secreto = 42
+    username = "Alan"
+
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
+
+    if (chute < 1 or chute > 100):
+        print("\nVocê deve digitar um número entre 1 e 100!")
+        continue
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("\nParabéns", username + ", você acertou!")
+        break
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif (menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
+    
+
+
+print("\nFim do jogo!\n")
